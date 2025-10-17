62252ebb0bdfe99b7b5bb95f2b12584932e7f7f0
italianrich <33686149+italianrich@users.noreply.github.com>
2025-10-17 17:02:35 +0000

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from '@astrojs/partytown';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [mdx(), sitemap(), icon(),partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+})],
   vite: {
     plugins: [tailwindcss()],
   },
